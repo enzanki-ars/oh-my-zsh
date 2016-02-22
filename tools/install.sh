c1706969cd91528c42c56a0b1992444f903234bc
Alex Shafer <shafer.alex@gmail.com>
2016-02-22 10:59:15 -0500

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -24,7 +24,7 @@ main() {
   # which may fail on systems lacking tput or terminfo
   set -e
 
-  CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
+  CHECK_ZSH_INSTALLED=$(command -v foo >/dev/null 2>&1)
   if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
     printf "${YELLOW}Zsh is not installed!${NORMAL} Please install zsh first!\n"
     exit
